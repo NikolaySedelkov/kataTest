1861f0d0faea9fffcf8b3f28b55e7c1ee8db33cf
NikolayS <nikolay.sedelkov@gmail.com>
2023-01-26 16:12:47 +0300

    p2

--- OnLyCode.java
+++ OnLyCode.java
@@ -1,30 +1,49 @@
 package org.example;
 
+import java.util.Collections;
+import java.util.NavigableMap;
+import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            System.out.println(calc("10+2"));
-        }catch (Exception e){
-            System.out.println(e.getMessage());
+        while(true) {
+            try {
+                System.out.println(calc(new Scanner(System.in).next()));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
     static String accepted = "-+*/";
-
     interface Action{
-        int compare(Integer a, Integer b);
+        Integer compare(Integer a, Integer b);
     }
     public static Integer parseNumber(String number){
         try {
             int i = Integer.parseInt(number);
+            if(i < 0 || i > 10) return null;
             return i;
         }catch (Exception e){
             return null;
         }
     }
-
+    private static final NavigableMap<Integer, String> units;
+    static {
+        NavigableMap<Integer, String> initMap = new TreeMap<>();
+        initMap.put(100, "C");
+        initMap.put(90, "XC");
+        initMap.put(50, "L");
+        initMap.put(40, "XL");
+        initMap.put(10, "X");
+        initMap.put(9, "IX");
+        initMap.put(5, "V");
+        initMap.put(4, "IV");
+        initMap.put(1, "I");
+        units = Collections.unmodifiableNavigableMap(initMap);
+    }
     public static Integer parseRoman(String number){
         if (number.equals("I"))     return 1;
         if (number.equals("II"))    return 2;
@@ -39,7 +58,21 @@ public class Main {
 
         return null;
     }
-    public static int calc(String input) throws Exception {
+
+    public static String parseRoman(Integer number) throws Exception {
+        if (number <= 0)
+            throw new Exception("No correctly answer");
+        StringBuilder result = new StringBuilder();
+        for(Integer key : units.descendingKeySet()) {
+            while (number >= key) {
+                number -= key;
+                result.append(units.get(key));
+            }
+        }
+        return result.toString();
+    }
+
+    public static String calc(String input) throws Exception {
         String output = input.replaceAll(" ", "");
         String left = null, right = null;
         int i;
@@ -75,19 +108,23 @@ public class Main {
         Integer leftInt = parseNumber(left);
         if(leftInt != null){
             Integer rightInt = parseNumber(right);
-            if(rightInt != null)    return a.compare(leftInt, rightInt);
-            else                    throw  new Exception("Parameters of different formats");
+            if(rightInt != null)    {
+                Integer result = a.compare(leftInt, rightInt);
+                if(result < 0)  throw new Exception("No correctly answer");
+                return          a.compare(leftInt, rightInt).toString();
+            }
+            else throw  new Exception("Parameters of different formats");
         }
         else{
             Integer leftRoman = parseRoman(left);
             if(leftRoman != null){
                 Integer rightRoman = parseRoman(right);
-                if(rightRoman != null)    return a.compare(leftRoman, rightRoman);
+                if(rightRoman != null)    return parseRoman(a.compare(leftRoman, rightRoman));
                 else                      throw  new Exception("Parameters of different formats");
             }
         }
 
-        throw  new Exception("Incorrect number");
+        throw  new Exception("Parameters of different formats");
     }
 
 }
